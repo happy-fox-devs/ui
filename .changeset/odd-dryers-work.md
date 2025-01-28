7a406be5d08fd7beb1d39a75655086f29197262c
AdrianY1997 <yasnoadriandatos@gmail.com>
2025-01-28 12:37:22 -0500

    refactor button component for enhanced type safety, update CSS variable naming, and improve build scripts; add changeset for patch version

---
+++ .changeset/odd-dryers-work.md
@@ -0,0 +1,5 @@
+---
+"@happy-fox/ui": patch
+---
+
+refactor button component: enhance type safety, update CSS variables, and improve build scripts
--- src/button/button.types.ts
+++ src/button/button.types.ts
@@ -3,28 +3,30 @@ import { HTMLAttributes, RefObject } from "react";
 import { DynamicCSSVariables } from "../global.types";
 
 type CustomCSSVariables = React.CSSProperties &
-  DynamicCSSVariables<"font-size" | "padding" | "color">;
+  DynamicCSSVariables<"font-size" | "padding" | "gap" | "bg-color" | "decorator-size">;
 
 type Variant = "solid" | "plain" | "soft" | "outlined";
 type Color = "primary" | "success" | "danger" | "warning" | "neutral" | "secondary" | "black";
 type Size = "3xs" | "2xs" | "xs" | "sm" | "nm" | "lg" | "xl";
 type Rounded = "none" | "xs" | "nm" | "lg" | "xl" | "full";
+type Target = "_self" | "_blank" | "_parent" | "_top" | "framename";
+type Type = "button" | "submit" | "reset";
 
 export type ButtonProps = {
-  variant?: Variant;
-  color?: Color;
-  size?: Size;
-  rounded?: Rounded;
-  icon?: boolean;
-  decorator?: LucideIcon | { icon: LucideIcon; pos: "left" | "right" };
-  loading?: boolean;
   as?: "button" | "a";
-  href?: string;
-  target?: "_blank" | "_self" | "_parent" | "_top" | "framename";
-  style?: CustomCSSVariables;
-  hover?: boolean;
-  type?: "button" | "submit";
+  color?: Color;
   disabled?: boolean;
+  decorator?: { icon: LucideIcon; pos?: "left" | "right" };
+  hover?: boolean;
+  href?: string;
+  icon?: boolean;
+  loading?: boolean;
   ref?: RefObject<HTMLButtonElement | null>;
+  rounded?: Rounded;
+  size?: Size;
+  style?: CustomCSSVariables;
+  target?: Target;
+  type?: Type;
+  variant?: Variant;
 } & HTMLAttributes<HTMLButtonElement> &
   HTMLAttributes<HTMLAnchorElement>;
